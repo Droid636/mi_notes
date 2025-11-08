70caf10f1eca9582124fcc426334acd4752502cb
Droid636 <2022043@utsh.edu.mx>
2025-11-07 21:41:15 -0600

    Dependencias de firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    id("com.google.gms.google-services")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -41,4 +42,14 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // Firebase BOM — asegura versiones compatibles
+    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
+
+    // SDKs que vas a usar
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
 }
